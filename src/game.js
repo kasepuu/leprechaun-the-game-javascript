abd0070fa7154be69f3ae7c115288d7e8b6515fd
juss <jussmartson21@gmail.com>
2023-05-12 20:43:48 +0300

    update

--- src/game.js
+++ src/game.js
@@ -1,69 +1,102 @@
-
-let jumpActive = false
-let start, previousTimeStamp;
-
+let isMovingLeft = false;
+let isMovingRight = false;
+let isJumping = false;
+let animationId = null;
 // main menu
 export const main = () => {
     let body = document.getElementById("playground")
     let Character = document.getElementById("character")
-
-    var currrent = Character.getBoundingClientRect();
-    console.log(currrent.y, body.offsetHeight + body.getBoundingClientRect().y - 39)
-
-    function movediv(timestamp) {
-        if (start === undefined) {
-            start = timestamp;
-        }
-        const elapsed = timestamp - start;
-
-        if (currrent.y <= body.offsetHeight + body.getBoundingClientRect().y - 39) {
-    
-            if (previousTimeStamp !== timestamp) {
-                const count = Math.min(0.1 * elapsed, 100);
-                Character.style.transform = `translateY(${count}px)`;
-            }
-            requestAnimationFrame(movediv);
-        }
-    }
-    if (!jumpActive) {
-        requestAnimationFrame(movediv)
-    }
-    function charJump(timestamp) {
-        if (start === undefined) {
-            start = timestamp;
-        }
-        const elapsed = timestamp - start;
-
-        if (previousTimeStamp !== timestamp) {
-            const count = Math.min(0.1 * elapsed, 100);
-            Character.style.transform = `translateY(${-count}px)`;
-            if (count === 100) jumpActive = false;
-        }
-        if (elapsed < 2000) { 
-            previousTimeStamp = timestamp;
-            if (jumpActive) {
-                window.requestAnimationFrame(charJump);
-            } 
-        }
-    }
+  
     document.addEventListener("keydown", (event) => {
         let currentPos = Character.getBoundingClientRect()
         let bodyPos = body.getBoundingClientRect()
 
-
-        if ((event.code === 'ArrowRight' || event.code === 'KeyD') && currentPos.x <= body.offsetWidth + bodyPos.x - 30) Character.style.left = currentPos.x + 10 + 'px'
-        else if ((event.code === 'ArrowRight' || event.code === 'KeyD')) Character.style.left = body.offsetWidth + bodyPos.x - 20
-
-        if ((event.code === 'ArrowLeft' || event.code === 'KeyA') && currentPos.x - 10 >= bodyPos.x) Character.style.left = currentPos.x - 10 + 'px'
-        else if ((event.code === 'ArrowLeft' || event.code === 'KeyA')) Character.style.left = bodyPos.x
-
-        if (event.code === 'Space') {
-            jumpActive = true
-            requestAnimationFrame(charJump)
-        }
-
+        if ((event.code === 'ArrowRight' || event.code === 'KeyD') && currentPos.x <= body.offsetWidth + bodyPos.x - 30) moveRight();
+        if ((event.code === 'ArrowLeft' || event.code === 'KeyA') && currentPos.x - 10 >= bodyPos.x) moveLeft();
+        if (event.code === 'Space') charJump();
+    });
+    document.addEventListener("keyup", (event) => {
+        if (event.code === 'ArrowRight' || event.code === 'KeyD' || event.code === 'ArrowLeft' || event.code === 'KeyA')  stopAnimation();
     });
 
     body.appendChild(Character)
     requestAnimationFrame(main)
+}
+
+
+function moveLeft() {
+    if (isMovingLeft) return;
+
+    isMovingLeft = true;
+  
+    function moveAnimation() {
+      const currentLeft = parseInt(character.style.left, 10) || 0;
+      const newLeft = currentLeft - 10;
+      character.style.left = newLeft + 'px';
+  
+      if (newLeft % 10 === 0) {
+        stopAnimation();
+      } else {
+        animationId = requestAnimationFrame(moveAnimation);
+      }
+    }
+  
+    animationId = requestAnimationFrame(moveAnimation);
+  }
+  
+  function stopAnimation() {
+    isMovingLeft = false;
+    isMovingRight = false;
+    cancelAnimationFrame(animationId);
+    animationId = null;
+  }
+
+  function moveRight() {
+    if (isMovingRight) return;
+
+    isMovingRight = true;
+  
+    function moveAnimation() {
+      const currentLeft = parseInt(character.style.left, 10) || 0;
+      const newLeft = currentLeft + 10;
+      character.style.left = newLeft + 'px';
+  
+      if (newLeft % 10 === 0) {
+        stopAnimation();
+      } else {
+        animationId = requestAnimationFrame(moveAnimation);
+      }
+    }
+  
+    animationId = requestAnimationFrame(moveAnimation);
+  }
+
+  function charJump() {
+    if (isJumping) return;
+
+    isJumping = true;
+    let currentJumpHeight = 0;
+
+    function jumpAnimation() {
+        currentJumpHeight += 10;
+        character.style.bottom = currentJumpHeight + 'px';
+
+        if (currentJumpHeight >= 100) {
+            fallAnimation();
+        } else {
+            requestAnimationFrame(jumpAnimation);
+        }
+    }
+
+    function fallAnimation() {
+        currentJumpHeight -= 10;
+        character.style.bottom = currentJumpHeight + 'px';
+
+        if (currentJumpHeight <= 0) {
+            isJumping = false;
+        } else {
+            requestAnimationFrame(fallAnimation);
+        }
+    }
+    requestAnimationFrame(jumpAnimation);
 }
