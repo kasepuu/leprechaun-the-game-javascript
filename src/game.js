043d4e7b14bcbf1e177b99f84fe3b381cd9de858
juss <jussmartson21@gmail.com>
2023-05-12 19:42:17 +0300

    update

--- src/game.js
+++ src/game.js
@@ -1,69 +1,55 @@
 
-let jumpDone = false
+let jumpActive = false
 let start, previousTimeStamp;
+
 // main menu
 export const main = () => {
     let body = document.getElementById("playground")
     let Character = document.getElementById("character")
-    //     let gameMenu = document.createElement("div")
-    //     let gameMenuBTN = document.createElement("button")
-    //     let gameMenuBTN2 = document.createElement("button")
 
-
-    //     body.style.background = "url(../images/main_menu_gamename.png)"
-    //     body.style.backgroundRepeat = "no-repeat"
-    //     body.style.backgroundSize = "cover"
-    //    // background-image: url("images/main_menu_gamename.png");
-    //     gameMenu.id = `mainMenu`
-
-    //     gameMenuBTN.textContent = "PLAY"
-    //     gameMenuBTN.id = "playButton"
-    //     gameMenuBTN2.textContent = "CREDITS"
-    //     gameMenuBTN2.id = "creditsButton"
-
-    //     gameMenu.classList.add("main")
-    //     gameMenu.style.color = "red"
-    //     gameMenu.textContent = "asad"
-
-    //     gameMenu.appendChild(gameMenuBTN)
-    //     gameMenu.appendChild(gameMenuBTN2)
-    //body.appendChild(Character)
     var currrent = Character.getBoundingClientRect();
-    console.log(currrent.y, body.offsetHeight - body.getBoundingClientRect().y)
+    console.log(currrent.y, body.offsetHeight + body.getBoundingClientRect().y - 39)
 
     function movediv(timestamp) {
-        if (currrent.y <= body.offsetHeight - body.getBoundingClientRect().y && currrent.y >= body.getBoundingClientRect().y) {
-            currrent.y--
-            console.log(currrent.y, body.getBoundingClientRect().y)
-            Character.style.bottom = currrent.y + 'px';     
+        if (start === undefined) {
+            start = timestamp;
+        }
+        const elapsed = timestamp - start;
+
+        if (currrent.y <= body.offsetHeight + body.getBoundingClientRect().y - 39) {
+    
+            if (previousTimeStamp !== timestamp) {
+                const count = Math.min(0.1 * elapsed, 100);
+                Character.style.transform = `translateY(${count}px)`;
+            }
             requestAnimationFrame(movediv);
         }
     }
-    requestAnimationFrame(movediv);
-
+    if (!jumpActive) {
+        requestAnimationFrame(movediv)
+    }
     function charJump(timestamp) {
         if (start === undefined) {
             start = timestamp;
-          }
-          const elapsed = timestamp - start;
-        
-          if (previousTimeStamp !== timestamp) {
+        }
+        const elapsed = timestamp - start;
+
+        if (previousTimeStamp !== timestamp) {
             const count = Math.min(0.1 * elapsed, 100);
             Character.style.transform = `translateY(${-count}px)`;
-            if (count === 200) jumpDone = true;
-          }
-          if (elapsed < 2000) {
-            // Stop the animation after 2 seconds
+            if (count === 100) jumpActive = false;
+        }
+        if (elapsed < 2000) { 
             previousTimeStamp = timestamp;
-            if (!jumpDone) {
-              window.requestAnimationFrame(charJump);
-            }
-          }
+            if (jumpActive) {
+                window.requestAnimationFrame(charJump);
+            } 
+        }
     }
     document.addEventListener("keydown", (event) => {
         let currentPos = Character.getBoundingClientRect()
         let bodyPos = body.getBoundingClientRect()
-        
+
 
         if ((event.code === 'ArrowRight' || event.code === 'KeyD') && currentPos.x <= body.offsetWidth + bodyPos.x - 30) Character.style.left = currentPos.x + 10 + 'px'
         else if ((event.code === 'ArrowRight' || event.code === 'KeyD')) Character.style.left = body.offsetWidth + bodyPos.x - 20
@@ -71,7 +57,10 @@ export const main = () => {
         if ((event.code === 'ArrowLeft' || event.code === 'KeyA') && currentPos.x - 10 >= bodyPos.x) Character.style.left = currentPos.x - 10 + 'px'
         else if ((event.code === 'ArrowLeft' || event.code === 'KeyA')) Character.style.left = bodyPos.x
 
-        if (event.code === 'Space') requestAnimationFrame(charJump)
+        if (event.code === 'Space') {
+            jumpActive = true
+            requestAnimationFrame(charJump)
+        }
 
     });
 
