050ab9a03031cd3660e5f9e7d367b3a10ea60db0
juss <jussmartson21@gmail.com>
2023-05-14 15:41:25 +0300

    update

--- src/game.js
+++ src/game.js
@@ -9,6 +9,7 @@ let body = document.getElementById("playground")
 let theGame = document.getElementById("theGame")
 let Character = document.getElementById("thePlayer")
 let Floors = document.getElementById("floors")
+let floorArr = [];
 import { level1 } from "./levels.js";
 let frameTimes = []
 var gameIsPaused = false
@@ -48,11 +49,11 @@ function main() {
     frameRate() // get Framerate
     timer() // handle Timer
 
-    let floor = document.getElementById("floor-1")
-    let floorHeight = parseInt(floor.style.bottom, 10);
+   // let floor = document.getElementById("floor-1")
+    //let floorHeight = parseInt(floor.style.bottom, 10);
 
-    if (Character.getBoundingClientRect().y === Floors.offsetHeight) isCollided = true
-    else if (Character.getBoundingClientRect().y === (floorHeight + 20)) isCollided = true
+    if (Character.getBoundingClientRect().y === 20) isCollided = true
+    //else if (Character.getBoundingClientRect().y === (floorHeight + 20)) isCollided = true
 
     else isCollided = false
     if (!isCollided && !isJumping) fallAnimation()
@@ -149,7 +150,7 @@ function charJump(startY) {
 
     isJumping = true;
     let currentJumpHeight = startY;
-    
+
     function jumpAnimation() {
         currentJumpHeight += 10;
         thePlayer.style.bottom = currentJumpHeight + 'px';
@@ -165,28 +166,40 @@ function charJump(startY) {
 }
 
 
+
+
+
+
 function fallAnimation() {
+    console.log(Character.style.left)
     let characterX = parseInt(Character.style.left, 10) + 25
     let characterY = parseInt(Character.style.bottom, 10);
-    let floor = document.getElementById("floor-1")
-    let floorMin = parseInt(floor.style.left, 10)
-    let floorMax = floorMin + floor.offsetWidth + 20;
-    let floorS = parseInt(Floors.style.bottom, 10)
-    let floorHeight = parseInt(floor.style.bottom, 10);
 
-    if (isCollided || characterY === floorS + Floors.offsetHeight || characterY <= 10
-        || (characterY === (floorHeight + floor.offsetHeight) && (characterX >= floorMin && characterX <= floorMax))
-    ) {
-        isCollided = true
-        if (!isMovingLeft && !isMovingRight) {
-            Character.style.backgroundImage = "url(/images/leprechaun.png)"
-        } else if (isMovingLeft && !isMovingRight) {
-            Character.style.backgroundImage = "url(/images/leprechaun_walking_LEFT.png)"
-        } else if (!isMovingLeft && isMovingRight) {
-            Character.style.backgroundImage = "url(/images/leprechaun_walking_RIGHT_gif.gif)"
+    if (isCollided || characterY <= 20
+        || (floorArr.includes(String(characterY) + "H"))) {
+        let floorMin, floorMax;
+        let num = floorArr.indexOf(String(characterY) + "H")
+
+        if (num === -1) {
+            floorMin = 9999
+            floorMax = 0
+        } else {
+            floorMin = parseInt(floorArr[num-2])
+            floorMax = parseInt(floorArr[num-1])
         }
+        
+        if (isCollided || characterY <= 20 || (characterX >= floorMin && characterX <= floorMax)) {
+            isCollided = true
+            if (!isMovingLeft && !isMovingRight) {
+                Character.style.backgroundImage = "url(/images/leprechaun.png)"
+            } else if (isMovingLeft && !isMovingRight) {
+                Character.style.backgroundImage = "url(/images/leprechaun_walking_LEFT.png)"
+            } else if (!isMovingLeft && isMovingRight) {
+                Character.style.backgroundImage = "url(/images/leprechaun_walking_RIGHT_gif.gif)"
+            }
 
-        return
+            return
+        }
     }
     characterY -= 10;
     thePlayer.style.bottom = characterY + 'px';
@@ -198,11 +211,23 @@ export function stopGame() {
 export function startGame() {
     gameRunning = true;
     Character.style.bottom = 30 + 'px';
-    level1()
+
+    level1();
+
+    let allFloors = Floors.getElementsByTagName('div');
+    for (var i = 1; i < allFloors.length; i++) {
+        let floorMin = String(parseInt(allFloors[i].style.left, 10)) + "Min";
+        let floorMax = parseInt(allFloors[i].style.left, 10) + allFloors[i].offsetWidth + 20 + "Max";
+        let floorHeight = parseInt(allFloors[i].style.bottom, 10) + allFloors[i].offsetHeight + "H";
+        floorArr.push(floorMin, floorMax, floorHeight);
+        console.log(floorArr)
+    }
+
     main()
 }
 
 export function resetGame() {
+    floorArr = []
     let thePlayer = document.getElementById("thePlayer")
     if (thePlayer) {
         thePlayer.removeAttribute("style")
--- src/levels.js
+++ src/levels.js
@@ -1,18 +1,36 @@
-export const level1 = () =>{
+export const level1 = () => {
     let theGame = document.getElementById("theGame")
     let floors = document.getElementById("floors")
-    let floor = document.createElement("div")
-    floor.id = "floor-1"
-    floor.style.backgroundColor = "cyan"
-    floor.style.height= "20px"
-    floor.style.width = "500px"
-    floor.style.position = "absolute"
-    floor.style.bottom = "120"
-    floor.style.left = "300"
-    floor.style.borderRadius = "5px"
 
-    floors.appendChild(floor)
-    floors.style.bottom  = 0 + 'px';
+    let bottomFloor = document.createElement("div")
+    bottomFloor.id = "bottomfloor"
+    bottomFloor.style.bottom = 0 + 'px';
+    bottomFloor.style.backgroundColor = "blue";
+    bottomFloor.style.width = "1080px";
+    bottomFloor.style.height = "20px";
+
+    let floor1 = document.createElement("div")
+    floor1.id = "floor-1"
+    floor1.style.width = "500px"
+    floor1.style.bottom = "120px"
+    floor1.style.left = "300px"
+
+    let floor2 = document.createElement("div")
+    floor2.id = "floor-2"
+    floor2.style.width = "200px"
+    floor2.style.bottom = "240px"
+    floor2.style.left = "100px"
+
+    let floor3 = document.createElement("div")
+    floor3.id = "floor-2"
+    floor3.style.width = "200px"
+    floor3.style.bottom = "360px"
+    floor3.style.left = "300px"
+
+    floors.appendChild(bottomFloor)
+    floors.appendChild(floor1)
+    floors.appendChild(floor2)
+    floors.appendChild(floor3)
     floors.removeAttribute("hidden")
     theGame.appendChild(floors);
 }
